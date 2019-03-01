f478603b151d33ccc93dcee756c439cf6ca80084
tonyastro77 <tonyastro77@users.noreply.github.com>
2019-03-01 09:58:54 +0200

    Adding REST services

--- Bookstore/src/main/java/fi/haagahelia/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/fi/haagahelia/Bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package fi.haagahelia.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.Bookstore.domain.Book;
 import fi.haagahelia.Bookstore.domain.BookRepository;
@@ -23,6 +27,18 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "bookList";
+	}
+	
+	// RESTful service to get all the books
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	// RESTful service to get book by id
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
 	}
 	
 	@RequestMapping(value = "/add")
